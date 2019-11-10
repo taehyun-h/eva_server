cc2f8d39544f4c28cdc706995a04dd59a0ba7749
Taehyun Hwang <taehyun95@gmail.com>
2019-11-10 14:55:53 +0900

    Implement singleton

---
+++ eva/utils/singleton.py
@@ -0,0 +1,11 @@
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    @property
+    def instance(self):
+        return self._instance
