6c015c58f05424c2ac906587eb04609881da84f3
Taehyun Hwang <taehyun95@gmail.com>
2019-11-11 14:41:03 +0900

    Add is_today_millisecnods function in time.py

--- eva/utils/time.py
+++ eva/utils/time.py
@@ -6,6 +6,11 @@ def get_current_time():
     return int(time.time())
 
 
+def is_today_milliseconds(timestamp):
+    timestamp = int(timestamp / 1000)
+    return is_today(timestamp)
+
+
 def is_today(timestamp):
     today = datetime.now()
     dt_object = datetime.fromtimestamp(timestamp)
