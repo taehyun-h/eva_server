8eb3db156529ff34035961cf9b385c771d0cf68a
Taehyun Hwang <taehyun95@gmail.com>
2019-11-19 13:24:00 +0900

    Implement study current word index in user

--- eva/protocol/data/user.py
+++ eva/protocol/data/user.py
@@ -83,9 +83,29 @@ class User(object):
 
             self.pb_object.last_studied_word_id = word_id
             if word_id not in self.pb_object.studying_word_orders:
-                word_order = self.pb_object.studying_word_orders[word_id]
-                word_order.id = word_id
-                word_order.order = self.pb_object.today_study_date
+                self.add_studying_word_order(word_id,
+                                             self.pb_object.today_study_date)
+            if word_id not in self.pb_object.studying_words:
+                self.add_studying_word(word_id)
+
+    def study_current_index(self):
+        current_index = self.pb_object.today_studying_words_index
+        current_word_id = self.pb_object.today_studying_word_ids[current_index]
+        if current_word_id in self.pb_object.studying_word_orders:
+            return
+        if current_word_id in self.pb_object.testing_word_orders:
+            return
+
+        studying_word = self.pb_object.studying_words[current_word_id]
+        studying_word.studied_count += 1
+        if studying_word.studied_count >= len(STUDY_PERIOD_DATE):
+            next_order = self.pb_object.today_study_date + TEST_PERIOD_DATE[0]
+            self.add_testing_word_order(current_word_id, next_order)
+            self.add_testing_word(current_word_id)
+        else:
+            next_order = self.pb_object.today_study_date + STUDY_PERIOD_DATE[
+                studying_word.studied_count]
+            self.add_studying_word_order(current_word_id, next_order)
 
     def is_first_sign_in_today(self):
         return not time.is_today_milliseconds(
@@ -93,6 +113,26 @@ class User(object):
 
     def is_adding_newly_studying_words(self):
         return self.pb_object.today_study_date % 2 == 1
+
+    def add_studying_word(self, word_id):
+        studying_word = self.pb_object.studying_words[word_id]
+        studying_word.id = word_id
+        studying_word.studied_count = 0
+
+    def add_testing_word(self, word_id):
+        testing_word = self.pb_object.testing_words[word_id]
+        testing_word.id = word_id
+        testing_word.passed_count = 0
+
+    def add_studying_word_order(self, word_id, order):
+        word_order = self.pb_object.studying_word_orders[word_id]
+        word_order.id = word_id
+        word_order.order = order
+
+    def add_testing_word_order(self, word_id, order):
+        word_order = self.pb_object.testing_word_orders[word_id]
+        word_order.id = word_id
+        word_order.order = order
 
     @property
     def id(self):
